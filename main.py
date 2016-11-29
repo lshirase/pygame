8f3a5678d739867849837a401e82607da80024ed
Len Shirase <lshirase@umich.edu>
2016-11-29 15:51:56 -0500

    cleaning up

--- main.py
+++ main.py
@@ -30,10 +30,10 @@ WIN_LEVEL = 20
 current_speed = 50 # Current tile speed in milliseconds
 board = []
 direction = 1
-current_x = 0
-current_y = BOARD_HEIGHT - 1
-old_y = current_y
-current_width = 3
+x_position = 0
+y_position = BOARD_HEIGHT - 1
+old_y = y_position
+block_width = 3
 current_level = 0
 col = (247, 202, 201)
 prevcol = 0
@@ -49,7 +49,7 @@ PLAYING = 1
 LOSE = 2
 WIN = 3
 
-game = INTRO
+game_status= INTRO
 
 images = ( pygame.image.load(os.path.join('backgrounds', 'first.png')), pygame.image.load(os.path.join('backgrounds', 'main.png')), pygame.image.load(os.path.join('backgrounds', 'lose.png')), pygame.image.load(os.path.join('backgrounds', 'youwon.png')))
 
@@ -57,7 +57,7 @@ images = ( pygame.image.load(os.path.join('backgrounds', 'first.png')), pygame.i
 play = True
 
 def main():
-	global game, current_x, current_y, current_speed, play, current_width, current_level, current_color, current_colorcount, goodsound
+	global game_status, x_position, y_position, current_speed, play, block_width, current_level, current_color, current_colorcount, goodsound
 	pygame.init()
 	pygame.mixer.init(frequency=22050, size=-16, channels=2, buffer=4096)
 	screen = pygame.display.set_mode( SCREEN_SIZE )
@@ -74,113 +74,115 @@ def main():
 		update_screen(screen)
 		
 		for event in pygame.event.get():
-			if event.type == pygame.QUIT: 
-				play = False
-			elif event.type == KEYDOWN:
+			if event.type == KEYDOWN:
 				if event.key == K_SPACE:
 					key_hit()
-				if event.key == K_F1: # Yes, this is a cheat.
-					current_x -= 1
-					if (current_x < 0): current_x = 0
-					current_width += 1
-					if (current_width >= BOARD_WIDTH): current_width = BOARD_WIDTH - 1
+				##bug checking, makes it easier to check win state
+				if event.key == K_RETURN: 
+					x_position -= 1
+					if (x_position < 0): 
+						x_position = 0
+					block_width += 1
+					if (block_width >= BOARD_WIDTH): 
+						block_width = BOARD_WIDTH - 1
 
 def reset_game():
-	global game, current_x, current_y, current_speed, play, current_width, current_level
+	global game_status, x_position, y_position, current_speed, play, block_width, current_level
 
 	clear_board()
 	
 	play = True
 	
-	game = INTRO
+	game_status= INTRO
 	
-	current_x = 0
-	current_y = BOARD_HEIGHT - 1
+	x_position = 0
+	y_position = BOARD_HEIGHT - 1
 	current_level = 0
 	current_speed = TILE_SPEED[current_level]
-	current_width = 3
+	block_width = 3
 
 
 def key_hit():
-	global play, game, current_x, current_y, current_width, current_speed, current_level
+	global play, game_status, x_position, y_position, block_width, current_speed, current_level
 	goodsound = pygame.mixer.Sound(os.path.join('sounds', 'good.ogg'))
 	badsound = pygame.mixer.Sound(os.path.join('sounds', 'bad.ogg'))
 	losesound = pygame.mixer.Sound(os.path.join('sounds', 'lose.ogg'))
-	tempwidth = current_width
-	if game == PLAYING:
-		if current_y < BOARD_HEIGHT - 1:
-			for x in range(current_x, current_x + current_width):
-				if board[x][current_y + 1] == 0: # If they're standing on a block that did not work
-					current_width -= 1 # Then next time, give them one less block
-					board[x][current_y] = 0 # Also, get rid of this block that isn't standing on solid ground.
+	tempwidth = block_width
+	if game_status== PLAYING:
+		if y_position < BOARD_HEIGHT - 1:
+			for x in range(x_position, x_position + block_width):
+				if board[x][y_position + 1] == 0: # If they're standing on a block that did not work
+					block_width -= 1 # Then next time, give them one less block
+					board[x][y_position] = 0 # Also, get rid of this block that isn't standing on solid ground.
 		##plays a good sound if player doesn't lose a block
-		if current_width == tempwidth:
+		if block_width == tempwidth:
 			goodsound.play()
+		##plays a bad sound if player loses a block
 		else:
 			badsound.play()
-		current_y -= 1
+		y_position -= 1
 		current_level += 1
-		game_status()
-		if game == LOSE:
+		win_lose()
+		if game_status== LOSE:
 			losesound.play()
-	elif game == INTRO:
-		game = PLAYING
-	elif game == LOSE:
+	elif game_status== INTRO:
+		game_status= PLAYING
+	elif game_status== LOSE:
 		reset_game()
-		game = INTRO
+		game_status= INTRO
 	else:
 		play = False
 
-def game_status():
-	global game, current_width, current_level, current_speed, play, TILE_COLOR
+def win_lose():
+	global game_status, block_width, current_level, current_speed, play, TILE_COLOR
 	
-	if current_width == 0:
-		game = LOSE
+	if block_width == 0:
+		game_status= LOSE
 	elif current_level == WIN_LEVEL:
-	 	game = WIN
+	 	game_status= WIN
 	else:
 		current_speed = TILE_SPEED[current_level]
 
 
 last_time = 0
 def update_movement():
-	global game, last_time, current_x, current_y, current_width, current_speed, direction
+	global game_status, last_time, x_position, y_position, block_width, current_speed, direction
 
-	if game == PLAYING:
+	if game_status== PLAYING:
 		current_time = pygame.time.get_ticks()
 		if (last_time + current_speed <= current_time):
-			new_x = current_x + direction
-			if (new_x < 0) or (new_x + current_width > BOARD_WIDTH):
+			new_x = x_position + direction
+			if (new_x < 0) or (new_x + block_width > BOARD_WIDTH):
 				direction = -direction
 			
-			current_x += direction
+			x_position += direction
 			
 			last_time = current_time
 		
 def update_screen(screen):
 	global game
 
-	screen.blit(images[game], (0,0,SCREEN_WIDTH,SCREEN_HEIGHT), (0,0,SCREEN_WIDTH,SCREEN_HEIGHT))
+	screen.blit(images[game_status], (0,0,SCREEN_WIDTH,SCREEN_HEIGHT), (0,0,SCREEN_WIDTH,SCREEN_HEIGHT))
 
-	if game == PLAYING:
-		if current_y < SCREEN_HEIGHT - 3:
-			screen.blit(images[game], (0,0,SCREEN_WIDTH,SCREEN_HEIGHT - 10), (0,0,SCREEN_WIDTH,SCREEN_HEIGHT - 10))
+	if game_status== PLAYING:
+		if y_position < SCREEN_HEIGHT - 3:
+			screen.blit(images[game_status], (0,0,SCREEN_WIDTH,SCREEN_HEIGHT - 10), (0,0,SCREEN_WIDTH,SCREEN_HEIGHT - 10))
 		draw_board(screen)
 		
 		# pygame.draw.rect(screen, col, (4 * TILE_WIDTH, BOARD_HEIGHT -3 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT))
 		# pygame.draw.rect(screen, (0, 0, 0), (4 * TILE_WIDTH, BOARD_HEIGHT -3 * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT), 2)
 
-	elif game == INTRO:
+	elif game_status== INTRO:
 		pass
-	elif game == LOSE:
+	elif game_status== LOSE:
 		pass
-	elif game == WIN:
+	elif game_status== WIN:
 		pass
 	
 	pygame.display.flip()
 	
 def update_board_info():
-	clear_row(current_y)
+	clear_row(y_position)
 	fill_current_row()
 	
 def draw_board(screen):
@@ -296,9 +298,9 @@ def clear_row(y):
 		board[x][y] = 0
 
 def fill_current_row():
-	global current_x, current_y, current_width
-	for x in range(current_x, current_x + current_width):
-		board[x][current_y] = 1
+	global x_position, y_position, block_width
+	for x in range(x_position, x_position + block_width):
+		board[x][y_position] = 1
 
 
 main()
