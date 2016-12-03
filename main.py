858f1ed872cea8088aec76e6f0eacbdd421a4e74
Len Shirase <lshirase@umich.edu>
2016-12-03 15:57:05 -0500

    block class finally woo

--- main.py
+++ main.py
@@ -36,11 +36,19 @@ WIN = 3
 
 images = ( pygame.image.load(os.path.join('backgrounds', 'first.png')), pygame.image.load(os.path.join('backgrounds', 'main.png')), pygame.image.load(os.path.join('backgrounds', 'lose.png')), pygame.image.load(os.path.join('backgrounds', 'youwon.png')))
 
+class Block(object):
+	def __init__(self):
+		self.x_position = 0
+		self.y_position = BOARD_HEIGHT - 1
+		self.col = (247, 202, 201)
+		self.block_width = 3
+
+
 class Scoreboard(pygame.sprite.Sprite):
 	def __init__(self):
 		pygame.sprite.Sprite.__init__(self)
 		self.rows = 20
-		self.font = pygame.font.SysFont("Helvetica", 18)
+		self.font = pygame.font.SysFont("Helvetica", 16)
         
 	def update(self):
 		self.text = "rows left %d" % (self.rows)
@@ -54,14 +62,7 @@ class Game(object):
 		self.current_speed = 50 
 		self.board = []
 		self.direction = 1
-		self.x_position = 0
-		self.y_position = BOARD_HEIGHT - 1
-		self.old_y = self.y_position
-		self.block_width = 3
 		self.current_level = 0
-		self.col = (247, 202, 201)
-		self.prevcol = 0
-		self.colorreverse = False
 		self.play = True
 		self.game_status= INTRO
 
@@ -74,18 +75,19 @@ class Game(object):
 		self.screen = pygame.display.set_mode( SCREEN_SIZE )
 		pygame.mixer.music.load(os.path.join('sounds', 'soundtrack.ogg'))
 		pygame.mixer.music.play(-1)
-
-		self.reset_game()
+		block = Block()
+		self.reset_game(block)
 		scoreboard = Scoreboard()
 		scoreSprite = pygame.sprite.Group(scoreboard)
+		
 
 		while(self.play):
 
 
 			
-			self.movement()
-			self.update_board_info()
-			self.update_screen(self.screen,scoreSprite)
+			self.movement(block)
+			self.update_board_info(block)
+			self.update_screen(self.screen,scoreSprite,block)
 			scoreSprite.update()
 			scoreSprite.draw(self.screen)
 
@@ -95,18 +97,18 @@ class Game(object):
 			for event in pygame.event.get():
 				if event.type == KEYDOWN:
 					if event.key == K_SPACE:
-						self.key_hit(scoreboard, scoreSprite)
+						self.key_hit(scoreboard, scoreSprite, block)
 						pygame.display.flip()
 
 
 					##bug checking, makes it easier to check win state
 					if event.key == K_RETURN: 
-						self.x_position -= 1
-						if (self.x_position < 0): 
-							self.x_position = 0
-						self.block_width += 1
-						if (self.block_width >= BOARD_WIDTH): 
-							self.block_width = BOARD_WIDTH - 1
+						block.x_position -= 1
+						if (block.x_position < 0): 
+							block.x_position = 0
+						block.block_width += 1
+						if (block.block_width >= BOARD_WIDTH): 
+							block.block_width = BOARD_WIDTH - 1
 
 			
 
@@ -116,44 +118,44 @@ class Game(object):
 
 
 
-	def movement(self):
+	def movement(self,block):
 
 		if self.game_status== PLAYING:
 			current_time = pygame.time.get_ticks()
 			if (self.last_time + self.current_speed <= current_time):
-				new_x = self.x_position + self.direction
-				if (new_x < 0) or (new_x + self.block_width > BOARD_WIDTH):
+				new_x = block.x_position + self.direction
+				if (new_x < 0) or (new_x + block.block_width > BOARD_WIDTH):
 					self.direction = -self.direction
 				
-				self.x_position += self.direction
+				block.x_position += self.direction
 				
 				self.last_time = current_time
 
-	def update_board_info(self):
-		self.clear_row(self.y_position)
-		self.fill_current_row()
+	def update_board_info(self,block):
+		self.clear_row(block)
+		self.fill_current_row(block)
 
 
-	def clear_row(self,y):
+	def clear_row(self,block):
 		for x in range(BOARD_WIDTH):
-			self.board[x][y] = 0
+			self.board[x][block.y_position] = 0
 
-	def fill_current_row(self):
-		self.x_position, self.y_position, self.block_width
-		for x in range(self.x_position, self.x_position + self.block_width):
-			self.board[x][self.y_position] = 1
+	def fill_current_row(self,block):
 
-	def update_screen(self,screen,scoreSprite):
+		for x in range(block.x_position, block.x_position + block.block_width):
+			self.board[x][block.y_position] = 1
+
+	def update_screen(self,screen,scoreSprite,block):
 
 
 		self.screen.blit(images[self.game_status], (0,0,SCREEN_WIDTH,SCREEN_HEIGHT), (0,0,SCREEN_WIDTH,SCREEN_HEIGHT))
 
 		if self.game_status== PLAYING:
-			if self.y_position < SCREEN_HEIGHT - 3:
+			if block.y_position < SCREEN_HEIGHT - 3:
 				self.screen.blit(images[self.game_status], (0,0,SCREEN_WIDTH,SCREEN_HEIGHT - 15), (0,0,SCREEN_WIDTH,SCREEN_HEIGHT - 15))
 				
 			scoreSprite.draw(self.screen)
-			self.draw_board(self.screen)
+			self.draw_board(self.screen,block)
 	
 
 
@@ -171,11 +173,11 @@ class Game(object):
 		
 		pygame.display.flip()
 
-	def draw_board(self,screen):
+	def draw_board(self,screen,block):
 		for x in range(BOARD_WIDTH):
 			for y in range(BOARD_HEIGHT):
 				if self.board[x][y] == 1:
-					self.draw_tile(self.screen, x, y)
+					self.draw_tile(self.screen, x, y,block)
 
 
 
@@ -183,55 +185,55 @@ class Game(object):
 
 
 				
-	def draw_tile(self,screen, x, y):
+	def draw_tile(self,screen, x, y,block):
 
-		if self.colorreverse == False:
-			try:
-				self.col = TILE_COlOR_LIST_ONE[self.prevcol + 1]
-			except:
-				self.colorreverse == True
-				self.col = TILE_COlOR_LIST_ONE[self.prevcol - 1]
+		# if self.colorreverse == False:
+		# 	try:
+		# 		block.col = TILE_COlOR_LIST_ONE[self.prevcol + 1]
+		# 	except:
+		# 		self.colorreverse == True
+		# 		self.col = TILE_COlOR_LIST_ONE[self.prevcol - 1]
 
-		else:
-			try:
-				self.col = TILE_COlOR_LIST_ONE[self.prevcol - 1]
-			except:
-				self.colorreverse == False
-				self.col = TILE_COlOR_LIST_ONE[self.prevcol + 1]
+		# else:
+		# 	try:
+		# 		self.col = TILE_COlOR_LIST_ONE[self.prevcol - 1]
+		# 	except:
+		# 		self.colorreverse == False
+		# 		self.col = TILE_COlOR_LIST_ONE[self.prevcol + 1]
 
 		if y == BOARD_HEIGHT - 1:
-			self.col = TILE_COlOR_LIST_ONE[0]
+			block.col = TILE_COlOR_LIST_ONE[0]
 		elif y == BOARD_HEIGHT - 2:
-			self.col = TILE_COlOR_LIST_ONE[1]
+			block.col = TILE_COlOR_LIST_ONE[1]
 		elif y == BOARD_HEIGHT - 3:
-			self.col = TILE_COlOR_LIST_ONE[2]
+			block.col = TILE_COlOR_LIST_ONE[2]
 		elif y == BOARD_HEIGHT - 4:
-			self.col = TILE_COlOR_LIST_ONE[3]
+			block.col = TILE_COlOR_LIST_ONE[3]
 		elif y == BOARD_HEIGHT - 5:
-			self.col = TILE_COlOR_LIST_ONE[4]
+			block.col = TILE_COlOR_LIST_ONE[4]
 		elif y == BOARD_HEIGHT - 6:
-			self.col = TILE_COlOR_LIST_ONE[5]
+			block.col = TILE_COlOR_LIST_ONE[5]
 		elif y == BOARD_HEIGHT - 7:
-			self.col = TILE_COlOR_LIST_ONE[6]
+			block.col = TILE_COlOR_LIST_ONE[6]
 		elif y == BOARD_HEIGHT - 8:
-			self.col = TILE_COlOR_LIST_ONE[5]
+			block.col = TILE_COlOR_LIST_ONE[5]
 		elif y == BOARD_HEIGHT - 9:
-			self.col = TILE_COlOR_LIST_ONE[4]
+			block.col = TILE_COlOR_LIST_ONE[4]
 		elif y == BOARD_HEIGHT - 10:
-			self.col = TILE_COlOR_LIST_ONE[3]
+			block.col = TILE_COlOR_LIST_ONE[3]
 		elif y == BOARD_HEIGHT - 11:
-			self.col = TILE_COlOR_LIST_ONE[2]
+			block.col = TILE_COlOR_LIST_ONE[2]
 		elif y == BOARD_HEIGHT - 12:
-			self.col = TILE_COlOR_LIST_ONE[1]
+			block.col = TILE_COlOR_LIST_ONE[1]
 		elif y == BOARD_HEIGHT - 13:
-			self.col = TILE_COlOR_LIST_ONE[0]
+			block.col = TILE_COlOR_LIST_ONE[0]
 
 
-		pygame.draw.rect(self.screen, self.col, (x * TILE_WIDTH, y * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT))
+		pygame.draw.rect(self.screen, block.col, (x * TILE_WIDTH, y * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT))
 		pygame.draw.rect(self.screen, (0, 0, 0), (x * TILE_WIDTH, y * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT), 2)
 
 
-	def reset_game(self):
+	def reset_game(self,block):
 
 		self.clear_board()
 		
@@ -239,42 +241,42 @@ class Game(object):
 		
 		self.game_status= INTRO
 		
-		self.x_position = 0
-		self.y_position = BOARD_HEIGHT - 1
+		block.x_position = 0
+		block.y_position = BOARD_HEIGHT - 1
 		self.current_level = 0
 		self.current_speed = TILE_SPEED[self.current_level]
-		self.block_width = 3
+		block.block_width = 3
 
 
-	def key_hit(self,scoreboard, scoreSprite):
+	def key_hit(self,scoreboard, scoreSprite, block):
 
 		goodsound = pygame.mixer.Sound(os.path.join('sounds', 'good.ogg'))
 		badsound = pygame.mixer.Sound(os.path.join('sounds', 'bad.ogg'))
 		losesound = pygame.mixer.Sound(os.path.join('sounds', 'lose.ogg'))
-		tempwidth = self.block_width
+		tempwidth = block.block_width
 		if self.game_status== PLAYING:
-			if self.y_position < BOARD_HEIGHT - 1:
-				for x in range(self.x_position, self.x_position + self.block_width):
-					if self.board[x][self.y_position + 1] == 0: # If they're standing on a block that did not work
-						self.block_width -= 1 # Then next time, give them one less block
-						self.board[x][self.y_position] = 0 # Also, get rid of this block that isn't standing on solid ground.
+			if block.y_position < BOARD_HEIGHT - 1:
+				for x in range(block.x_position, block.x_position + block.block_width):
+					if self.board[x][block.y_position + 1] == 0: # If they're standing on a block that did not work
+						block.block_width -= 1 # Then next time, give them one less block
+						self.board[x][block.y_position] = 0 # Also, get rid of this block that isn't standing on solid ground.
 			##plays a good sound if player doesn't lose a block
-			if self.block_width == tempwidth:
+			if block.block_width == tempwidth:
 				goodsound.play()
 			##plays a bad sound if player loses a block
 			else:
 				badsound.play()
-			self.y_position -= 1
+			block.y_position -= 1
 			scoreboard.rows -=1
 			self.current_level += 1
-			self.win_lose()
+			self.win_lose(block)
 			if self.game_status== LOSE:
 				losesound.play()
 				scoreboard.rows = WIN_LEVEL
 		elif self.game_status== INTRO:
 			self.game_status= PLAYING
 		elif self.game_status== LOSE:
-			self.reset_game()
+			self.reset_game(block)
 			self.game_status= INTRO
 		else:
 			self.play = False
@@ -282,10 +284,10 @@ class Game(object):
 
 
 
-	def win_lose(self):
+	def win_lose(self,block):
 
 		
-		if self.block_width == 0:
+		if block.block_width == 0:
 			self.game_status= LOSE
 		elif self.current_level == WIN_LEVEL:
 		 	self.game_status= WIN
